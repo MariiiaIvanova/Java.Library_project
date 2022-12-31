8676e0a2a4812f531b45698cbfc319713399698d
nkondratieva <shadaorg@gmail.com>
2022-12-31 22:30:21 +0300

    fix ClientPanel

--- My_labrary_project/Client/src/ru/library/My_library_project/client/ClientPanel.java
+++ My_labrary_project/Client/src/ru/library/My_library_project/client/ClientPanel.java
@@ -8,15 +8,11 @@ import java.awt.event.ActionListener;
 public class ClientPanel extends JPanel {
     public ClientPanel() {
 
-        JPanel clientPanel = new JPanel(new BorderLayout());
-
-
         JButton findButton = new JButton("Find book");
         JButton seeButton = new JButton("See all");
 
-        clientPanel.add(findButton);
-        clientPanel.add(seeButton, BorderLayout.EAST);
+        add(findButton);
+        add(seeButton, BorderLayout.EAST);
 
-        clientPanel.setVisible(true);
     }
 }
